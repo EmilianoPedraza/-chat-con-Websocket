7fc60f2eabeb7d9e73724a93b43c9d17a4bbb415
emipedraza <j.p.e.23.11.98@gmail.com>
2022-11-28 22:00:56 -0300

    completa la parte 1

--- public/index.js
+++ public/index.js
@@ -12,29 +12,35 @@ const productoHtml = (prod)=>{
 
 const listProdcuts = (arrayProductos) => {
     const listaProductos = document.querySelector(".listaProductos");
+
     const h3mensaje = document.createElement("h3")
-    h3mensaje.innerText = "no hay productos</h3"
-    h3mensaje.className = "text-light text-center notProducts"
-    const notProduct = document.querySelector(".notProducts");
-    if(arrayProductos.length > 0){
-        notProduct.remove()
-        listaProductos.innerHTML = `
-               <div class="d-flex flex-column align-items-center divProdListContainer">
-                   <div class="col-8">
-                       <table class="table table-dark tablaProductos">
-                           <tr class="table-active">
-                               <th scope="col">Nombre</th>
-                               <th scope="col">Precio</th>
-                               <th scope="col">link img</th>
-                           </tr>
-                       </table>
-                   </div>
-               </div>`
-         const productos = arrayProductos.forEach((prod)=>{
-            productoHtml(prod)
-         })
+         h3mensaje.innerHTML = "no hay productos"
+         h3mensaje.className = "text-light text-center notProducts"
          
+         h3mensaje.className = "text-light text-center notProducts"
+         if(arrayProductos.length > 0){
+            const divContainer = document.createElement("div")
+                divContainer.className = "d-flex flex-column align-items-center divProdListContainer"
+                h3mensaje.remove()
+                divContainer.innerHTML = `
+                    <div class="col-8">
+                        <table class="table table-dark tablaProductos">
+                            <tr class="table-active">
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">link img</th>
+                            </tr>
+                        </table>
+                    </div>`
+                listaProductos.appendChild(divContainer)
+                arrayProductos.forEach((prod)=>{
+                    productoHtml(prod)
+                })
     }
+    else{
+        listaProductos.appendChild(h3mensaje)
+    }
+
 };
 
 
